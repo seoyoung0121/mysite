02a0b31e2e19344208478d852c73c79297a1acfe
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-12-31 09:07:31 +0900

    mysite03 - refactor: 강사님 코드로 AuthInterceptor 변경

--- mysite03/src/main/java/mysite/security/AuthInterceptor.java
+++ mysite03/src/main/java/mysite/security/AuthInterceptor.java
@@ -14,47 +14,46 @@ public class AuthInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// 1. Handler 종류 확인
-		if(!(handler instanceof HandlerMethod)) {
-			// DefaultServletRequestHandler 타입인 경우 
+		if (!(handler instanceof HandlerMethod)) {
+			// DefaultServletRequestHandler 타입인 경우
 			// DefaultServletHandler가 처리하는 경우(정적 자원, /assets/**, mapping이 안되어 있는 URL)
 			return true;
 		}
-		
+
 		// 2. casting
 		HandlerMethod handlerMethod = (HandlerMethod) handler;
-		
+
 		// 3. Handler에서 @Auth
 		Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
-		
+
 		// 4. Handler Method에서 @Auth가 없으면 클래스(타입)에 가져오기!!!
-		if(auth==null) {
-			auth = handlerMethod.getBeanType().getAnnotation(Auth.class);
+		if (auth == null) {
+			auth = handlerMethod.getMethod().getDeclaringClass().getAnnotation(Auth.class);
 		}
-		
+
 		// 5. @Auth가 없으면 ...
-		if(auth==null) {
+		if (auth == null) {
 			return true;
 		}
-		
-		String role = auth.role();
-		
+
 		// 5. @Auth가 붙어 있기 때문에 인증(Authentication) 여부 확인
 		HttpSession session = request.getSession();
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		
-		if(authUser==null) {
-			response.sendRedirect(request.getContextPath()+"/user/login");
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
+
+		if (authUser == null) {
+			response.sendRedirect(request.getContextPath() + "/user/login");
 			return false;
 		}
-		
-		
-		if("ADMIN".equals(role)&& !authUser.getRole().equals("ADMIN")) {
+
+		String role = auth.role();
+
+		if ("ADMIN".equals(role) && !"ADMIN".equals(authUser.getRole())) {
 			response.sendRedirect(request.getContextPath());
 			return false;
 		}
-		
-		// 5. @Auth가 붙어있고 인증도 된 경우  
+
+		// 5. @Auth가 붙어있고 인증도 된 경우
 		return true;
 	}
-	
+
 }
