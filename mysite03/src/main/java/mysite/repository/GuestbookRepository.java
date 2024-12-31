8ba6f15a7d1ee27831c630f4fe757cea84707451
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-12-31 16:50:41 +0900

    mysite03 - feat: user, guestbook mybatis 적용

--- mysite03/src/main/java/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/mysite/repository/GuestbookRepository.java
@@ -1,25 +1,15 @@
 package mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sql.DataSource;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
-	@Autowired
-	private DataSource dataSource;
 	
 	private SqlSession sqlSession;
 	
@@ -28,70 +18,15 @@ public class GuestbookRepository {
 	}
 	
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<>();
-		
-		try (Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc");
-			ResultSet rs = pstmt.executeQuery();
-			){
-			
-			while (rs.next()) {
-				Long id = rs.getLong(1);
-				String name = rs.getString(2);
-				String contents = rs.getString(3);
-				String regDate=rs.getString(4);
-
-				GuestbookVo vo = new GuestbookVo();
-				vo.setId(id);
-				vo.setName(name);
-				vo.setContents(contents);
-				vo.setRegDate(regDate);
-				result.add(vo);
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		return result;
+		return sqlSession.selectList("guestbook.findAll");
 	}
 	
 	public int insert(GuestbookVo vo) {
-		int count = 0;
-
-
-		try (Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("insert into guestbook values(null, ?, ?, ?, now())");
-			){
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getContents());
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		return count;
-		
+		return sqlSession.insert("guestbook.insert", vo);
 	}
 	
 	public int deleteByIdAndPassword(Long id, String password) {
-		int count = 0;
-		
-		try (Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("delete from guestbook where id = ? and password = ?");
-			){
-
-			pstmt.setLong(1, id);
-			pstmt.setString(2, password);
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		return count;
-		
+		return sqlSession.delete("guestbook.deleteByIdAndPassword", Map.of("id", id, "password", password));
 	}
 	
 }
--- mysite03/src/main/java/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/mysite/repository/UserRepository.java
@@ -1,11 +1,6 @@
 package mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -26,96 +21,19 @@ public class UserRepository {
 		this.sqlSession=sqlSession;
 	}
 	public int insert(UserVo vo) {
-		int count = 0;
-
-		try (Connection conn = dataSource.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("insert into user values(null, ?, ?, ?, ?, now(),'USER')");) {
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		return count;
-
+		return sqlSession.insert("user.insert", vo);
 	}
 
 	public UserVo findByEmailAndPassword(String email, String password) {
-		UserVo userVo = null;
-
-		try (Connection conn = dataSource.getConnection();
-				PreparedStatement pstmt = conn
-						.prepareStatement("select id, name, role from user where email=? and password=?");) {
-
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				Long id = rs.getLong(1);
-				String name = rs.getString(2);
-				String role = rs.getString(3);
-
-				userVo = new UserVo();
-				userVo.setId(id);
-				userVo.setName(name);
-				userVo.setRole(role);
-			}
-			rs.close();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		return userVo;
+		return sqlSession.selectOne("user.findByEmailAndPassword", Map.of("email", email, "password", password));
 	}
 
 	public UserVo findById(Long id) {
-		UserVo userVo = null;
-
-		try (Connection conn = dataSource.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("select email, name, gender from user where id=?");) {
-
-			pstmt.setLong(1, id);
-
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-
-				String email = rs.getString(1);
-				String name = rs.getString(2);
-				String gender = rs.getString(3);
-
-				userVo = new UserVo();
-				userVo.setEmail(email);
-				userVo.setName(name);
-				userVo.setGender(gender);
-			}
-			rs.close();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		return userVo;
+		return sqlSession.selectOne("user.findById", id);
 	}
 
 	public void updateById(UserVo vo) {
-		String sql = "";
-		if (vo.getPassword().isEmpty()) {
-			sql = "update user set name = '" + vo.getName() + "', gender='" + vo.getGender() + "' where id=" + vo.getId();
-		} else {
-			sql = "update user set name = '" + vo.getName() + "', gender='" + vo.getGender() + "', password='"
-					+ vo.getPassword() + "' where id=" + vo.getId();
-		}
-		try (Connection conn = dataSource.getConnection(); 
-			Statement stmt = conn.createStatement();) {
-
-			stmt.executeUpdate(sql);
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
+		sqlSession.update("user.update", vo);
 
 	}
 
