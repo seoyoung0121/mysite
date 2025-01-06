9cde5d67e5d30bd553dd0c067a416a505e3d513f
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-06 16:00:32 +0900

    mysite03 - feat: 시간 재기

--- mysite03/src/main/java/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/mysite/repository/UserRepository.java
@@ -7,13 +7,12 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
-	@Autowired
-	private DataSource dataSource;
 	
 	private SqlSession sqlSession;
 	
@@ -25,7 +24,13 @@ public class UserRepository {
 	}
 
 	public UserVo findByEmailAndPassword(String email, String password) {
-		return sqlSession.selectOne("user.findByEmailAndPassword", Map.of("email", email, "password", password));
+		StopWatch sw = new StopWatch();
+		sw.start();
+		UserVo userVo=sqlSession.selectOne("user.findByEmailAndPassword", Map.of("email", email, "password", password));
+		sw.stop();
+		long totalTime=sw.getTotalTimeMillis();
+		System.out.println("[Exceution Time][UserRepository.findByEmailAndPassword] " + totalTime + "milisecond");
+		return userVo;
 	}
 
 	public UserVo findById(Long id) {
