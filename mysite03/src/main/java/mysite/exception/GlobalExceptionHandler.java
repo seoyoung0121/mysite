fb6f542a78fd2a9a91099c90e02e21c43abd2d59
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-13 09:47:27 +0900

    mysite03 - refactor: GlobalExceptionHandler 수정

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -9,19 +9,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String handler(Model model, Exception e) {
+	public void handler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
 		// 1. logging
-		StringWriter errors = new StringWriter();;
+		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
 		
-		// 2. 사과 페이지(종료)
-		model.addAttribute("errors", errors.toString());
-		return "errors/exception";
+		// 2. 요청 구분
+		// json 요청:request header의 accept: applicaion/json (o)
+		// html 요청: request header의 accept: applicaion/json (x)
+		
+		String accept = request.getHeader("accept");
+		
+		if(accept.matches(".*applicaion/json.*")) {
+			// 3. JSON 응답
+		} else {
+			// 4. 사과 페이지(종료)
+			request.setAttribute("errors", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request, response);
+		}
 	}
 }
