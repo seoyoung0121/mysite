08cf5fa9797a8952ffee1cfa444858d46d95f973
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-13 14:49:59 +0900

    mysite04 - mvc, web config

--- mysite04/src/main/java/mysite/interceptor/SiteInterceptor.java
+++ mysite04/src/main/java/mysite/interceptor/SiteInterceptor.java
@@ -1,17 +1,20 @@
 package mysite.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mysite.service.SiteService;
+import mysite.vo.SiteVo;
 
 public class SiteInterceptor implements HandlerInterceptor {
+	@Autowired
 	private LocaleResolver localeResolver;
 	
-	public SiteInterceptor(LocaleResolver localeResolver){
-		this.localeResolver=localeResolver;
-	}
+	@Autowired
+	private SiteService siteService;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -19,6 +22,12 @@ public class SiteInterceptor implements HandlerInterceptor {
 		String lang = localeResolver.resolveLocale(request).getLanguage();
 		request.setAttribute("lang", lang);
 		
+		SiteVo siteVo=(SiteVo)request.getServletContext().getAttribute("siteVo");
+		if(siteVo==null) {
+			siteVo= siteService.getSite();
+			request.getServletContext().setAttribute("siteVo", siteVo);
+		}
+
 		return true;
 	}
 	
