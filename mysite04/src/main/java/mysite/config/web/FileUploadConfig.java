f4360ad0958ee5b7ecadf4fa3dd441de4da7d9fd
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-14 09:39:21 +0900

    mysite04 - finish

--- mysite04/src/main/java/mysite/config/web/FileUploadConfig.java
+++ mysite04/src/main/java/mysite/config/web/FileUploadConfig.java
@@ -1,7 +1,10 @@
 package mysite.config.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,7 +13,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@PropertySource("classpath:mysite/config/web/fileupload.properties")
 public class FileUploadConfig implements WebMvcConfigurer{
+	@Autowired
+	private Environment env;
 	
 	// Multipart Resolver
 	@Bean
@@ -20,7 +26,7 @@ public class FileUploadConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/assets/upload-images/**").addResourceLocations("file:/Users/apple/mysite-uploads");
+		registry.addResourceHandler(env.getProperty("fileupload.resourceUrl")+"/**").addResourceLocations("file:"+env.getProperty("fileupload.uploadLocation")+"/");
 	}
 	
 }
