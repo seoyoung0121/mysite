79b18401b01f6461d504e4d2a069e44115c5da89
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-14 11:49:58 +0900

    mysite05 - refactor: config 변경

--- mysite05/src/main/java/mysite/config/web/MvcConfig.java
+++ mysite05/src/main/java/mysite/config/web/MvcConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -65,12 +66,17 @@ public class MvcConfig implements WebMvcConfigurer{
 		converters.add(mappingJackson2HttpMessageConverter());
 	}
 
+	// static(assets) url mapping
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:assets/");
+	}
 	
 	// DefaultServlet Handler
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
 	
 	// ApplicationContextEventListener
 	@Bean
@@ -87,6 +93,6 @@ public class MvcConfig implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(siteInterceptor()).addPathPatterns("/**").excludePathPatterns("/assets/**");
-	}
+	}	
 	
 }
