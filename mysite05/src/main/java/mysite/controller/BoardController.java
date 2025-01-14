883cb1a3b1ae09313f6205e2204fcbef92e4c4a2
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-14 10:49:43 +0900

    mysite05 - initial setting

---
+++ mysite05/src/main/java/mysite/controller/BoardController.java
@@ -0,0 +1,114 @@
+package mysite.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import mysite.security.Auth;
+import mysite.security.AuthUser;
+import mysite.service.BoardService;
+import mysite.vo.BoardVo;
+import mysite.vo.UserVo;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	private BoardService boardService;
+	
+	public BoardController(BoardService boardService) {
+		this.boardService=boardService;
+	}
+	
+	@RequestMapping
+	public String list(@RequestParam(name="page", required=false) String pageParam, Model model){
+		int page = 1;
+		if (pageParam != null && !pageParam.isEmpty()) {
+			page = Integer.parseInt(pageParam);
+			page = (page < 1) ? 1 : page;
+		}	
+		
+		Map<String, Object> map = boardService.getContentsList(page, null);
+		model.addAttribute("list", map.get("list"));
+		model.addAttribute("currentPage", map.get("currentPage"));
+		model.addAttribute("startPage", map.get("startPage"));
+		model.addAttribute("totalPage", map.get("totalPage"));
+		model.addAttribute("startArticleNum", map.get("startArticleNum"));
+		return "board/list";
+	}
+	
+	@RequestMapping("/view")
+	public String view(@RequestParam("id") Long id, Model model) {
+		BoardVo boardVo = boardService.getContents(id);
+		boardService.addHit(id);
+		model.addAttribute("vo", boardVo);
+		return "board/view";
+	}
+	
+	@RequestMapping(value="/write", method=RequestMethod.GET)
+	public String write(HttpSession session) {
+		// Access control
+		UserVo authUser= (UserVo)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/board";
+		}
+		
+		return "board/write";
+	}
+	
+	@Auth
+	@RequestMapping(value="/write", method=RequestMethod.POST)
+	public String write(@AuthUser UserVo authUser, BoardVo boardVo) {
+		if(authUser == null) {
+			return "redirect:/board";
+		}
+		
+		boardVo.setUserId(authUser.getId());
+		boardService.addContents(boardVo);
+		return "redirect:/board";
+	}
+	
+	@Auth
+	@RequestMapping("/delete")
+	public String delete(@AuthUser UserVo authUser, @RequestParam("id") Long id) {
+		
+		if(authUser == null) {
+			return "redirect:/board";
+		}
+		
+		boardService.deleteContents(id, authUser.getId());
+		
+		return "redirect:/board";
+	}
+	
+	@Auth
+	@RequestMapping(value="/modify", method=RequestMethod.GET)
+	public String modify(@AuthUser UserVo authUser, @RequestParam("id") Long id, Model model) {
+		
+		if(authUser == null) {
+			return "redirect:/board";
+		}
+		BoardVo vo=boardService.getContents(id, authUser.getId());
+		if(vo==null) {
+			return "redirect:/board";
+		}
+		model.addAttribute("vo", vo);
+		return "board/modify";
+	}
+	
+	@Auth
+	@RequestMapping(value="/modify", method=RequestMethod.POST)
+	public String modify(@AuthUser UserVo authUser, BoardVo boardVo) {
+		if(authUser == null || authUser.getId()!=boardVo.getUserId()) {
+			return "redirect:/board";
+		}
+		boardService.updateContents(boardVo);
+		return "redirect:/board/view?id="+boardVo.getId();
+	}
+	
+	
+}
---
+++ mysite05/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package mysite.exception;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mysite.dto.JsonResult;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
+	
+	@ExceptionHandler(Exception.class)
+	public void handler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
+		// 1. logging
+		StringWriter errors = new StringWriter();
+		e.printStackTrace(new PrintWriter(errors));
+		log.error(errors.toString());
+		
+		// 2. 요청 구분
+		// json 요청:request header의 accept: applicaion/json (o)
+		// html 요청: request header의 accept: applicaion/json (x)
+		
+		String accept = request.getHeader("accept");
+		
+		if(accept.matches(".*applicaion/json.*")) {
+			// 3. JSON 응답 
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application/json; chatset=utf-8");
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("utf-8"));
+			os.close();
+		} else {
+			// 4. 사과 페이지(종료)
+			request.setAttribute("errors", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request, response);
+		}
+	}
+}
