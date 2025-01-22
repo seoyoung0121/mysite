865a903fca2b82732265e48a2e3d1073b8779551
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-22 09:57:58 +0900

    mysite06 - fix: locale 오류 수정

--- mysite06/src/main/java/mysite/config/web/LocaleConfig.java
+++ mysite06/src/main/java/mysite/config/web/LocaleConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 @Configuration
 public class LocaleConfig {
 	// Locale Resolver
-	//@Bean
+	@Bean
 	public LocaleResolver localeResolver() {
 		CookieLocaleResolver localeResolver = new CookieLocaleResolver("lang");
 		localeResolver.setCookieHttpOnly(false);
